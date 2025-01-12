bf50536ce0447f9025b860feb9e6fb4ca922eaf5
Tiago <tiagorockman@gmail.com>
2025-01-12 19:50:43 -0300

    color text adjust and added defer

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,8 +1,5 @@
-
-window.onload = function() {
     const currentYear = new Date().getFullYear();
     document.getElementById('currentyear').appendChild(document.createTextNode(currentYear));
 
     const lastModifiedDate = new Date(document.lastModified);
     document.getElementById('lastModified').appendChild(document.createTextNode(lastModifiedDate));
-}
